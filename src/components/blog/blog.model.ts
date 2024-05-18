a656e5b8b7ba4cf593341d8f5d11381791d583c4
pragmaticAweds <jimohafeez738@gmail.com>
2024-05-18 21:19:37 +0100

    chore: setup backend for fs open

--- src/components/blog/blog.model.ts
+++ src/components/blog/blog.model.ts
@@ -1,17 +1,36 @@
-import { Schema } from "mongoose";
+import { Schema, model } from "mongoose";
 import { BlogAttributes } from "./blog.types";
 import { DocCounterAttributes } from "../../types";
 
-const blogSchema = new Schema<BlogAttributes>({
-  _id: String,
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-  User: { type: String, ref: "User" },
-  ref: { type: Number, default: 0 },
+const blogSchema = new Schema<BlogAttributes>(
+  {
+    _id: String,
+    title: String,
+    author: String,
+    url: String,
+    likes: Number,
+    User: { type: String, ref: "User" },
+    ref: { type: Number, default: 0 },
+  },
+  { timestamps: true }
+);
+
+const blogCounterSchema = new Schema<DocCounterAttributes>(
+  {
+    lastId: { type: Number, default: 0 },
+  },
+  { timestamps: true }
+);
+
+blogSchema.pre<BlogAttributes>("save", async function (next) {
+  if (this.isNew) {
+  }
+
+  next();
 });
 
-const blogCounterSchema = new Schema<DocCounterAttributes>({
-  lastId: { type: Number, default: 0 },
-});
+const BlogModel = model("Blog", blogSchema);
+
+export const BlogCounterModel = model("BlogCounter", blogCounterSchema);
+
+export default BlogModel;
