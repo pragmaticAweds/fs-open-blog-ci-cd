7d1ca8f1f133050099073ef0bebc91e77b5a6cb4
pragmaticAweds <jimohafeez738@gmail.com>
2024-05-27 22:32:19 +0100

    feat: setiinh up test for BE with test

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,9 +4,12 @@ import { env } from "process";
 
 dotenv.config();
 
+console.log(env.PORT);
+
 const appConfig = {
+  isTesting: env.NODE_ENV === "test",
   port: env.PORT || 5000,
-  db_url: env.DB_URL || "",
+  db_url: env.NODE_ENV === "test" ? env.TEST_DB_URI : env.DB_URI,
   authConfig: {
     jwtSecret: env.JWT_SECRET || "secret",
     saltRound: 10,
