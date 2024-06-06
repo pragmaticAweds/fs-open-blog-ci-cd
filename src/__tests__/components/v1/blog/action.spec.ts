6f6150e3c973e043bd93ef9b814d947efb9f4404
pragmaticAweds <jimohafeez738@gmail.com>
2024-06-06 12:08:13 +0100

    chore: fix race conditions when writing creating new blog test

--- src/__tests__/components/v1/blog/action.spec.ts
+++ src/__tests__/components/v1/blog/action.spec.ts
@@ -30,6 +30,13 @@ afterAll(async () => {
 });
 
 describe("Get blogs", () => {
+  beforeAll(async () => {
+    await createNewBlogs();
+  });
+
+  afterAll(async () => {
+    await BlogModel.deleteMany();
+  });
   test("all Blogs are returned as json", async () => {
     await api
       .get("/api/blogs")
@@ -37,121 +44,119 @@ describe("Get blogs", () => {
       .expect("Content-Type", /application\/json/);
   });
 
-  test.only("fetch a single blog successfully", async () => {
-    await createNewBlogs();
+  test("fetch a single blog", async () => {
+    await api.get("/api/blogs/1").expect(404);
 
-    // await api.get("/api/blogs/1").expect(404);
-
-    // await api.get("/api/blogs/000001").expect(200);
+    await api.get("/api/blogs/000001").expect(200);
   });
 });
 
-// describe("Create new blog", () => {
-//   let headers = "";
+describe("Creating new blog", () => {
+  let headers = "";
 
-//   const newBlog = newBlogs[1];
+  const newBlog = newBlogs[1];
 
-//   const collections = [
-//     UserModel,
-//     UserAccessModel,
-//     BlogModel,
-//   ] as unknown as Model<unknown>[];
+  const collections = [
+    UserModel,
+    UserAccessModel,
+    BlogModel,
+  ] as unknown as Model<unknown>[];
 
-//   const newUser = {
-//     username: "john_doe",
-//     password: "John.Doe1",
-//     name: "john doe",
-//     is_creator: true,
-//   };
+  const newUser = {
+    username: "john_doe",
+    password: "John.Doe1",
+    name: "john doe",
+    is_creator: true,
+  };
 
-//   const loginUser = {
-//     username: "john_doe",
-//     password: "John.Doe1",
-//   };
+  const loginUser = {
+    username: "john_doe",
+    password: "John.Doe1",
+  };
 
-//   beforeAll(async () => {
-//     await api.post("/api/auth/signup").send(newUser);
+  beforeAll(async () => {
+    await api.post("/api/auth/signup").send(newUser);
 
-//     const { body } = await api.post("/api/auth/login").send(loginUser);
+    const { body } = await api.post("/api/auth/login").send(loginUser);
 
-//     headers = `Bearer ${body.data.token}`;
-//   });
+    headers = `Bearer ${body.data.token}`;
+  });
 
-//   afterAll(async () => {
-//     await removeDbCollections(collections);
-//   });
+  afterAll(async () => {
+    await removeDbCollections(collections);
+  });
 
-//   test("verify new blog is created successfully", async ({ expect }) => {
-//     const { body } = await api
-//       .post("/api/blogs")
-//       .set("authorization", headers)
-//       .send(newBlog)
-//       .expect(201)
-//       .expect("Content-Type", /application\/json/);
+  test("verify new blog is created successfully", async ({ expect }) => {
+    const { body } = await api
+      .post("/api/blogs")
+      .set("authorization", headers)
+      .send(newBlog)
+      .expect(201)
+      .expect("Content-Type", /application\/json/);
 
-//     const blogs = await blogsInDb();
+    const blogs = await blogsInDb();
 
-//     expect(blogs.length).toEqual(4);
+    expect(blogs.length).toEqual(1);
 
-//     expect(body.data.title).toEqual(newBlog.title);
-//     expect(body.data.author).toEqual(newBlog.author);
+    expect(body.data.title).toEqual(newBlog.title);
+    expect(body.data.author).toEqual(newBlog.author);
 
-//     assert.strictEqual(body.data.url, newBlog.url);
-//   });
+    assert.strictEqual(body.data.url, newBlog.url);
+  });
 
-//   test("Only authorized personnel can create blog", async ({ expect }) => {
-//     await api.post("/api/blogs").send(newBlog).expect(401);
-//   });
+  test("Only authorized personnel can create blog", async ({ expect }) => {
+    await api.post("/api/blogs").send(newBlog).expect(401);
+  });
 
-//   test("ref and _id field is equal to last document count", async () => {
-//     const {
-//       body: { data },
-//     } = await api
-//       .post("/api/blogs")
-//       .set("authorization", headers)
-//       .send(newBlog);
+  test("ref and _id field is equal to last document count", async () => {
+    const {
+      body: { data },
+    } = await api
+      .post("/api/blogs")
+      .set("authorization", headers)
+      .send(newBlog);
 
-//     const blogTracker = await BlogCounterModel.findOne();
+    const blogTracker = await BlogCounterModel.findOne();
 
-//     const lastBlogId = blogTracker?.lastId;
+    const lastBlogId = blogTracker?.lastId;
 
-//     assert.strictEqual(data.ref, lastBlogId);
+    assert.strictEqual(data.ref, lastBlogId);
 
-//     assert.isTrue(data._id.endsWith(String(lastBlogId)));
-//   });
+    assert.isTrue(data._id.endsWith(String(lastBlogId)));
+  });
 
-//   test("Incomplete blog info returns 400 Bad Request", async ({ expect }) => {
-//     const { title, author, url } = newBlog;
+  test("Incomplete blog info returns 400 Bad Request", async ({ expect }) => {
+    const { title, author, url } = newBlog;
 
-//     await api
-//       .post("/api/blogs")
-//       .set("authorization", headers)
-//       .send({ title, author })
-//       .expect(400);
+    await api
+      .post("/api/blogs")
+      .set("authorization", headers)
+      .send({ title, author })
+      .expect(400);
 
-//     await api
-//       .post("/api/blogs")
-//       .set("authorization", headers)
-//       .send({ url, author })
-//       .expect(400);
+    await api
+      .post("/api/blogs")
+      .set("authorization", headers)
+      .send({ url, author })
+      .expect(400);
 
-//     await api
-//       .post("/api/blogs")
-//       .set("authorization", headers)
-//       .send({ url, title })
-//       .expect(400);
-//   });
+    await api
+      .post("/api/blogs")
+      .set("authorization", headers)
+      .send({ url, title })
+      .expect(400);
+  });
 
-//   test("Verify Likes should be empty array by default", async ({ expect }) => {
-//     const { body } = await api
-//       .post("/api/blogs")
-//       .set("authorization", headers)
-//       .send(newBlog);
+  test("Verify Likes should be empty array by default", async ({ expect }) => {
+    const { body } = await api
+      .post("/api/blogs")
+      .set("authorization", headers)
+      .send(newBlog);
 
-//     const blog = body.data;
+    const blog = body.data;
 
-//     expect(blog).toHaveProperty("likes");
+    expect(blog).toHaveProperty("likes");
 
-//     assert.deepEqual(blog.likes, []);
-//   });
-// });
+    assert.deepEqual(blog.likes, []);
+  });
+});
