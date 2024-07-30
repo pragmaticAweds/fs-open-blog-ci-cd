8865e04d0c12b74689d2061a77f59043c0767404
pragmaticAweds <jimohafeez738@gmail.com>
2024-07-30 21:32:04 +0100

    chore: rectifying pipeline environment

--- client/playwright.config.ts
+++ client/playwright.config.ts
@@ -75,6 +75,6 @@ export default defineConfig({
   webServer: {
     command: "npm run dev",
     url: "http://localhost:5173/",
-    // reuseExistingServer: !process.env.CI,
+    reuseExistingServer: !process.env.CI,
   },
 });
