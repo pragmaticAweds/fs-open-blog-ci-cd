ff6584d1563042de1c5e587d0a27e1985d957de2
pragmaticAweds <jimohafeez738@gmail.com>
2024-08-02 05:33:35 +0100

    chore: rectifying vite

--- server/src/app.ts
+++ server/src/app.ts
@@ -26,6 +26,7 @@ const allowedOrigin = [
   "http://localhost:5173",
   "http://localhost:5174",
   "http://localhost:3001",
+  "https://fs-open-blog-ci-cd.onrender.com",
 ];
 
 const corsOption = {
